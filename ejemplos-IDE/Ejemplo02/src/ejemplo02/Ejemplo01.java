6f1d4ba9f3888090050f82aeace5e1a419ff590b
FernandoArmijos98 <fjarmijos1@utpl.edu.ec>
2022-10-26 12:59:11 -0500

    creacion ejemplo2

--- ejemplos-IDE/Ejemplo02/src/ejemplo02/Ejemplo01.java
+++ ejemplos-IDE/Ejemplo02/src/ejemplo02/Ejemplo01.java
@@ -22,10 +22,14 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese su nombre");
+        System.out.println("Ingrese su nombre por favor");
         String nombre = entrada.nextLine();
-        System.out.println("Nombre ingresado es:" + nombre);
         
+        System.out.println("Ingrese su apellido por favor");
+        String apellido = entrada.nextLine(); 
+        
+        System.out.println("Nombre ingresado es:" + nombre);
+        System.out.println("Apellido ingresado es:" + apellido);
     }
     
 }
